839d0be09cda20ff5baad9fcf12afec7673406db
Max Brosnahan <maximilianbrosnahan@gmail.com>
2020-11-15 16:00:48 +0100

    Expand test to login

--- rails_app/spec/system/login_spec.rb
+++ rails_app/spec/system/login_spec.rb
@@ -1,9 +1,19 @@
 require "rails_helper"
 
 RSpec.describe "Logging in to the system" do
+  fixtures :users
+
+  let(:user) { users(:mctester) }
+
   scenario "the user logs in" do
     visit "/"
 
     expect(page).not_to have_content("Hello")
+    fill_in "Email", with: user.email
+    fill_in "Password", with: "password"
+    click_on "Log in"
+
+    expect(page).to have_content("Hello")
+    expect(page).to have_content(user.email)
   end
 end
